8a2487f65c06dd48372ac2cfcb4b5c6ac7bf1702
anasM0hammad <anas.1633.m@gmail.com>
2019-01-30 01:35:12 +0530

    added void states to texts animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,7 +36,34 @@ import { trigger , state , style , transition , animate } from '@angular/animati
            
        	])
  
-     ])
+     ]),
+
+
+     trigger('list1' , [
+           
+           state('in' , style({
+           	  opacity: '1',
+           	  transform: 'translateX(0)'
+           })),
+
+           transition('void => *' , [ 
+              style({
+                 opacity: '0',
+                 transform: 'translateX(-100px)'    	
+              }),
+
+              animate(400)
+
+           	]),
+
+           transition('* => void' , [
+              animate(400 ,  style({
+              	opacity: '0',
+              	transform: 'translateX(100px)'
+              }))
+           	])
+         
+     	])
   ]
 })
 export class AppComponent {
