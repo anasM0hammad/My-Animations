94a744dfdf896bff1b0167f038c954c5300002ad
anasM0hammad <anas.1633.m@gmail.com>
2019-01-29 23:25:50 +0530

    changing state with transition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,38 @@
 import { Component , ViewChild , ElementRef } from '@angular/core';
+import { trigger , state , style , transition , animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+     trigger('divState' , [
+       
+       state('normal' , style({
+       	 backgroundColor: 'blue' ,
+       	 borderRadius: '50%',
+       	 transform: 'translateX(0px)' 
+       })),
+    
+       state('highlighted' , style({
+       	 backgroundColor: 'red' ,
+       	  borderRadius: '0px',
+       	 transform: 'translateX(150px)'
+       })),
+
+       transition('normal => highlighted' , animate(300)),
+       transition('highlighted => normal' , animate(600))
+ 
+     ])
+  ]
 })
 export class AppComponent {
   
   allText: string[] = ['Anas' , 'Saad'] ;
   @ViewChild('input') text: ElementRef ;
-  
+
+  state = 'normal' ;
+
 
    onDelete(index: number){
      this.allText.splice(index, 1);
@@ -22,6 +45,10 @@ export class AppComponent {
 	     this.allText.push(text) ;
 	     this.text.nativeElement.value = '';
 	 }
+  }
+
+  onAnimate(){
+  	this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
   }
 
  
